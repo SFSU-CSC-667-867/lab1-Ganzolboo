5e476c7a13a061948841b5ef9e1808a19c10a0b5
Ganzolboo Ayurzana <a.ganzolboo@yahoo.com>
2019-02-06 21:06:51 -0800

    last

--- node/nextTick.js
+++ node/nextTick.js
@@ -1,4 +1,10 @@
-setImmediate(() => console.log('Now'));
 process.nextTick(() => console.log('Hello'));
+
+setImmediate(() => console.log('Now'));
+setImmediate(() => console.log('Now1'));
+
+setImmediate(() => console.log('Now2'));
+
+setImmediate(() => console.log('Now3'));
 
 console.log('world');
